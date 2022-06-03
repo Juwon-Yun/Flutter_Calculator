fdf09ad76d3153e57e956db6bf1bd9e28e7c920b
Juwon-Yun <eooll2@daum.net>
2022-06-03 18:01:35 +0900

    Feat: feature getter setter

--- lib/view_model/calculator_provider.dart
+++ lib/view_model/calculator_provider.dart
@@ -1,11 +1,54 @@
 import 'package:flutter/material.dart';
 
-class CalculatorProvider extends ChangeNotifier{
-  bool isEquation = true;
-  String calContent = '';
-  String answer = '';
-  List<String> operations = [];
-  List<String> calculations = [];
+class CalculatorProvider extends ChangeNotifier {
+  bool _isEquation = true;
+  String _calContent = '';
+  String _answer = '';
+  List<String> _operations = [];
+  List<String> _calculations = [];
 
+  bool get isEquation => _isEquation;
+  String get calContent => _calContent;
+  String get answer => _answer;
+  List<String> get operations => _operations;
+  List<String> get calculations => _calculations;
 
+  void setIsEquation() {
+    _isEquation = true;
+  }
+
+  void setIsNotEquation() {
+    _isEquation = false;
+  }
+
+  void addOperations(String text) {
+    _operations.add(text);
+  }
+
+  void addCalculations(String text) {
+    _calculations.add(text);
+  }
+
+  void attachCalContent(String text) {
+    _calContent += text;
+  }
+
+  void initCalContent() {
+    _calContent = '';
+  }
+
+  void substituteAnswer(String text) {
+    _answer = text;
+  }
+
+  void initAnswer() {
+    _answer = '';
+  }
+
+  bool isEmpty(String text) {
+    if (text == "") {
+      return true;
+    }
+    return false;
+  }
 }
