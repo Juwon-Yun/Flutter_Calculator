cc1ac4c78671e120986758a27924a7ebbd310967
Juwon-Yun <eooll2@daum.net>
2022-06-03 18:36:12 +0900

    Feat: attach provider

--- lib/main.dart
+++ lib/main.dart
@@ -51,80 +51,140 @@ class _CalculatorWidgetState extends State<CalculatorWidget> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              const _Header(),
-              _Body(calContent: calContent, answer: answer),
+              _Header(),
+              _Body(),
               Expanded(
-                child: GridView.builder(
-                    itemCount: Calculations.calculatorButtonRows.length,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 6),
-                    itemBuilder: (BuildContext context, index) {
-                      String buttonText =
-                          Calculations.calculatorButtonRows[index];
-                      return ElevatedButton(
-                        onPressed: () {
-                          if (Calculations.operations.contains(buttonText)) {
-                            if (calContent == "") return;
-                            setState(() {
-                              operations.add(buttonText);
-                              calContent += " $buttonText ";
-                            });
-                            return;
-                          }
+                child: Consumer<CalculatorProvider>(
+                  builder: (context, calculatorProvider, child) =>
+                      GridView.builder(
+                          itemCount: Calculations.calculatorButtonRows.length,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 6),
+                          itemBuilder: (BuildContext context, index) {
+                            String buttonText =
+                                Calculations.calculatorButtonRows[index];
+                            return ElevatedButton(
+                              onPressed: () {
+                                if (Calculations.operations
+                                    .contains(buttonText)) {
+                                  // if (calContent == "") return;
+                                  if (Provider.of<CalculatorProvider>(context)
+                                          .calContent ==
+                                      "") return;
+                                  // setState(() {
+                                  //   operations.add(buttonText);
+                                  //   calContent += " $buttonText ";
+                                  // });
+                                  Provider.of<CalculatorProvider>(context)
+                                      .addOperations(buttonText);
+                                  Provider.of<CalculatorProvider>(context)
+                                      .attachCalContent(' $buttonText ');
+                                  return;
+                                }
 
-                          if (buttonText == Calculations.clear) {
-                            setState(() {
-                              operations.add(Calculations.clear);
-                              calContent = "";
-                              answer = "";
-                            });
-                            return;
-                          }
+                                if (buttonText == Calculations.clear) {
+                                  // setState(() {
+                                  //   operations.add(Calculations.clear);
+                                  //   calContent = "";
+                                  //   answer = "";
+                                  // });
+                                  Provider.of<CalculatorProvider>(context)
+                                      .addOperations(Calculations.clear);
+                                  Provider.of<CalculatorProvider>(context)
+                                      .initCalContent();
+                                  Provider.of<CalculatorProvider>(context)
+                                      .initAnswer();
+                                  return;
+                                }
 
-                          if (buttonText == Calculations.equal) {
-                            String newCalContent =
-                                Calculator.parseString(calContent);
+                                if (buttonText == Calculations.equal) {
+                                  // String newCalContent =
+                                  //     Calculator.parseString(calContent);
+                                  String newCalContent = Calculator.parseString(
+                                      Provider.of<CalculatorProvider>(context)
+                                          .calContent);
 
-                            setState(() {
-                              if (newCalContent != calContent) {
-                                calculations.add(calContent);
-                              }
-                              operations.add(Calculations.equal);
-                              answer = newCalContent;
-                              isEquation = false;
-                            });
-                            return;
-                          }
+                                  // setState(() {
+                                  //   if (newCalContent != calContent) {
+                                  //   calculations.add(calContent);
+                                  // calculations.add(calContent);
+                                  // operations.add(Calculations.equal);
+                                  // answer = newCalContent;
+                                  // isEquation = false;
+                                  // });
 
-                          if (buttonText == Calculations.period) {
-                            return setState(() {
-                              calContent = Calculator.addPeriod(calContent);
-                            });
-                          }
+                                  if (newCalContent !=
+                                      Provider.of<CalculatorProvider>(context)
+                                          .calContent) {
+                                    Provider.of<CalculatorProvider>(context)
+                                        .addCalculations(
+                                            Provider.of<CalculatorProvider>(
+                                                    context)
+                                                .calContent);
+                                  }
+                                  Provider.of<CalculatorProvider>(context)
+                                      .addOperations(Calculations.equal);
+                                  Provider.of<CalculatorProvider>(context)
+                                      .substituteAnswer(newCalContent);
+                                  Provider.of<CalculatorProvider>(context)
+                                      .setIsNotEquation();
+                                  return;
+                                }
 
-                          setState(() {
-                            if (!isEquation &&
-                                operations.isNotEmpty &&
-                                operations.last == Calculations.equal) {
-                              answer = buttonText;
-                              isEquation = true;
-                            } else {
-                              calContent += buttonText;
-                            }
-                          });
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.grey[400],
-                          maximumSize: const Size(80, 80),
-                        ),
-                        child: Text(
-                          Calculations.calculatorButtonRows[index],
-                          style: const TextStyle(
-                              fontWeight: FontWeight.w700, fontSize: 20),
-                        ),
-                      );
-                    }),
+                                if (buttonText == Calculations.period) {
+                                  // return setState(() {
+                                  //   calContent =
+                                  //       Calculator.addPeriod(calContent);
+                                  // });
+                                  Provider.of<CalculatorProvider>(context)
+                                      .attachCalContent(Calculator.addPeriod(
+                                          Provider.of<CalculatorProvider>(
+                                                  context)
+                                              .calContent));
+                                  return;
+                                }
+
+                                // setState(() {
+                                //   if (!isEquation &&
+                                //       operations.isNotEmpty &&
+                                //       operations.last == Calculations.equal) {
+                                //     answer = buttonText;
+                                //     isEquation = true;
+                                //   } else {
+                                //     calContent += buttonText;
+                                //   }
+                                // });
+                                if (!Provider.of<CalculatorProvider>(context)
+                                        .isEquation &&
+                                    Provider.of<CalculatorProvider>(context)
+                                        .operations
+                                        .isNotEmpty &&
+                                    Provider.of<CalculatorProvider>(context)
+                                            .operations
+                                            .last ==
+                                        Calculations.equal) {
+                                  Provider.of<CalculatorProvider>(context)
+                                      .substituteAnswer(buttonText);
+                                  Provider.of<CalculatorProvider>(context)
+                                      .setIsEquation();
+                                } else {
+                                  Provider.of<CalculatorProvider>(context)
+                                      .attachCalContent(buttonText);
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.grey[400],
+                                maximumSize: const Size(80, 80),
+                              ),
+                              child: Text(
+                                Calculations.calculatorButtonRows[index],
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.w700, fontSize: 20),
+                              ),
+                            );
+                          }),
+                ),
               ),
             ],
           ),
@@ -137,42 +197,39 @@ class _CalculatorWidgetState extends State<CalculatorWidget> {
 class _Body extends StatelessWidget {
   const _Body({
     Key? key,
-    required this.calContent,
-    required this.answer,
   }) : super(key: key);
-
-  final String calContent;
-  final String answer;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       flex: 3,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Flexible(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text(
-                  calContent,
-                  style: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 20),
-                ),
-                Text(
-                  answer,
-                  style: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 60),
-                ),
-              ],
-            ),
-          )
-        ],
+      child: Consumer<CalculatorProvider>(
+        builder: (context, calculatorProvider, child) => Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Flexible(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    Provider.of<CalculatorProvider>(context).calContent,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 20),
+                  ),
+                  Text(
+                    Provider.of<CalculatorProvider>(context).answer,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 60),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
